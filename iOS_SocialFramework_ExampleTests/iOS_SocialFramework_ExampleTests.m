dfa2f17916ed9f3ebbf31e02b8e6f59188332d2c
ChungYeul In <sldk86@naver.com>
2014-01-21 13:43:35 +0900

    Initial Commit

---
+++ iOS_SocialFramework_ExampleTests/iOS_SocialFramework_ExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS_SocialFramework_ExampleTests.m
+//  iOS_SocialFramework_ExampleTests
+//
+//  Created by SDT-1 on 2014. 1. 21..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_SocialFramework_ExampleTests : XCTestCase
+
+@end
+
+@implementation iOS_SocialFramework_ExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
